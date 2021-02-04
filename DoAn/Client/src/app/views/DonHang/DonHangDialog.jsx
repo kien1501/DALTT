76847991922d063316b6d43622b084e9ae9d3730
kien1501 <mchv4400>
2021-02-04 13:55:26 +0700

    333333

--- DoAn/Client/src/app/views/DonHang/DonHangDialog.jsx
+++ DoAn/Client/src/app/views/DonHang/DonHangDialog.jsx
@@ -266,12 +266,12 @@ class DonHangDialog extends Component {
         field: "productColor.color.name",
         width: "300",
         headerStyle: {
-          minWidth: "150px",
+          minWidth: "200px",
           paddingLeft: "10px",
           paddingRight: "0px",
         },
         cellStyle: {
-          minWidth: "150px",
+          minWidth: "200px",
           paddingLeft: "10px",
           paddingRight: "0px",
           textAlign: "left",
